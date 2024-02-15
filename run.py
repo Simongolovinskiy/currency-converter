0722061156eb4357ab1aab507ae7e81c6d31e10b
unknown <tositrent164@gmail.com>
2024-02-15 17:34:53 +0300

    Added primitive logging and error handling, added some feature like virtual assistant chatGPT :zap:

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
+import os
 import logging
 import asyncio
 
 from src.bot.main import run_bot
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+log_file_path = os.path.join(project_dir, "bot_logs.log")
+logging.basicConfig(filename=log_file_path, level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
