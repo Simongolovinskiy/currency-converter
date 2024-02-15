1c03ea8160c8f3128c68efeeaf9dbd80eb8ad5d5
unknown <tositrent164@gmail.com>
2024-02-15 15:22:26 +0300

    implemented base converter func and bot :tada:

---
+++ src/api/converter.py
@@ -0,0 +1,16 @@
+import requests
+from src.settings.settings import SettingsManager
+
+
+class ConverterAPI:
+    def __init__(self):
+        self.__api_key = SettingsManager.get_converter_api_key()
+        self.base_url = SettingsManager.get_converter_url()
+
+    def convert(self, amount=100, base_cur="EUR", new_cur="RUB",):
+
+        response = requests.get(
+            f"{self.base_url}/{self.__api_key}/pair/{base_cur}/{new_cur}/{amount}"
+        )
+        return response.json()
+
